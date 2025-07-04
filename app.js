6810ee9c5ff81afcb6e11aebabdce8873872dd1e
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2025-07-04 00:07:28 -0500

    feat: :lock: add HTTPS support to backend and Vite frontend using self-signed certs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config({ path: './.env' });
+const fs = require('fs');
+const https = require('https');
 const ConnectToDatabase = require('./server/infraestructure/database/mongodb');
 const createServer = require('./server/infraestructure/server/server');
-
 
 const startApp = async () => {
     const connectToDatabase = new ConnectToDatabase();
@@ -9,8 +10,13 @@ const startApp = async () => {
 
     const app = createServer();
 
-    app.listen({ port: process.env.EXPRESS_PORT, host: process.env.EXPRESS_HOST }, () => {
-        console.log(`🚀 Servidor corriendo en: http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`);
+    const sslOptions = {
+        key: fs.readFileSync('./certs/key.pem'),
+        cert: fs.readFileSync('./certs/cert.pem')
+    };
+
+    https.createServer(sslOptions, app).listen(process.env.EXPRESS_PORT, process.env.EXPRESS_HOST, () => {
+        console.log(`🚀 Servidor corriendo en: https://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`);
     });
 };
 
--- server/infraestructure/server/server.js
+++ server/infraestructure/server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 
 const menusRoute = require('../../application/routes/menusRoute');
 const restaurantesRoute = require('../../application/routes/restauranteRoute');
+
 const createServer = () => {
     const app = express();
-    
-    app.use(express.json());   
+
+    // 🛡️ Middleware CORS
+    app.use(cors({
+        origin: 'https://localhost:5173', 
+        credentials: true
+    }));
+
+    app.use(express.json());
+
     // RUTAS
     app.use('/api', menusRoute);
     app.use('/seats', restaurantesRoute);
-    
-    
+
     return app;
 };
 
-
 module.exports = createServer;
-
-
