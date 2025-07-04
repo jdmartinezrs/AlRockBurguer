798f2059b2fd07c36955dff1c1a854fe8a22bfba
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2025-07-04 06:03:42 -0500

    feat: :sparkles: Configure React client with Vite and react-router-dom

---
+++ client/src/pages/SalonProvenza.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../css/salonProvenza.css';
+const SalonesProvenza = () => {
+
+
+
+    return (
+
+        <div className='title'>
+            <p>Hello World</p>
+        </div>
+
+    );
+
+
+};
+
+export default SalonesProvenza;
