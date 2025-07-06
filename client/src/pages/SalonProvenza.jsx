c5b1aca7fb7377cbbf901ce38b176a2cd36da64e
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2025-07-06 00:13:27 -0500

    feat: :children_crossing: Add visual layout for salon1 and tables

--- client/src/pages/SalonProvenza.jsx
+++ client/src/pages/SalonProvenza.jsx
@@ -7,9 +7,64 @@ const SalonesProvenza = () => {
 
     return (
 
-        <div className='title'>
-            <p>Hello World</p>
+    <div className="container">
+      <div className="despacho"></div>
+      <div className="tarima"></div>
+
+      <div className="sala1a">
+        <div className='MesaIzquierda1'>
+            <div className='Mesa1'></div>
         </div>
+        <div className='Mesa2'></div>
+      </div>
+
+      <div className="sala1b">
+        <div className="Horizontal1">
+             <div className='Mesa3'></div>
+        </div>
+       
+
+        <div className='vertical1'>
+            <div className='Mesa4'></div>
+        <div className='Mesa5'></div>
+        </div>
+        
+      </div>
+
+
+      <div className="sala1c">
+           <div className="Horizontal2">
+             <div className='Mesa6'></div>
+        </div>
+       
+
+        <div className='vertical2'>
+            <div className='Mesa7'></div>
+        <div className='Mesa8'></div>
+        </div>
+
+      </div>
+
+
+      <div className="sala1d">
+          
+            <div className='Mesa9'></div>
+        <div className='Mesa10'></div>
+       
+        
+
+
+      </div>
+
+
+
+      <div className="sala2a"></div>
+      <div className="sala2b"></div>
+      <div className="sala2c"></div>
+      <div className="sala2d"></div>
+    </div>
+
+
 
     );
 
